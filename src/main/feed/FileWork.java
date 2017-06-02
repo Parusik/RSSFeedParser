18a08a328faf69a69a93bf14f017423ffbeb2c17
Olya <olya.levchuck@gmail.com>
2017-06-02 04:18:42 +0300

    removed some methods from file class

--- src/main/feed/FileWork.java
+++ src/main/feed/FileWork.java
@@ -10,15 +10,9 @@ import java.util.*;
  * Created by Оля on 01.06.2017.
  */
 public class FileWork {
-    File file;
-    String path;
-    public FileWork (String s) {
-        this.path = s;
-        file = new File(path);
-    }
 
-
-    public List<String>  getListFromFile() {
+    public List<String>  getListFromFile(String path) {
+        File file = new File(path);
         Scanner sc = null;
         try {
             sc = new Scanner(file);
@@ -34,11 +28,6 @@ public class FileWork {
         return result;
 
     }
-    public void addInFile(String toAdd) {
-        try {
-            Files.write(Paths.get(path), toAdd.getBytes(), StandardOpenOption.APPEND);
-        } catch (IOException e) {
-            System.out.println("Error in adding a new source");
-        }
-    }
+
+
 }
--- src/main/feed/ToStart.java
+++ src/main/feed/ToStart.java
@@ -6,11 +6,11 @@ import java.util.*;
  * Created by Оля on 02.06.2017.
  */
 public class ToStart {
-    public static void start(){
-        FileWork fromFile = new FileWork("C:\\Users\\Оля\\IdeaProjects\\FEEDS\\src\\main\\feed\\rss.txt");
+    public static void start() {
+        FileWork fromFile = new FileWork();
         WorkWithXML xml = new WorkWithXML();
-        List<String> rss = fromFile.getListFromFile();
-        for(int i=0; i<rss.size();i++)
+        List<String> rss = fromFile.getListFromFile("C:\\Users\\Оля\\IdeaProjects\\FEEDS\\src\\main\\feed\\rss.txt");
+        for (int i = 0; i < rss.size(); i++)
             xml.start(rss.get(i));
     }
 }
